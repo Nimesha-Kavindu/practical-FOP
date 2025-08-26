9bf44fbd68df67a57a5cd1ca46599c0abdcd10e1
Nimesha Kavindu <fc115568@foc.sjp.ac.lk>
2025-08-26 05:58:13 +0530

    items above 500

--- program.cpp
+++ program.cpp
@@ -13,6 +13,59 @@ void displayVegetableInfo(int count, Vegetable veg[]) {
     }
 }
 
+void addVegetable(Vegetable veg[], int& count, int id, double price) {
+    if (count < 10) {
+        veg[count].itemID = id;
+        veg[count].pricePerKg = price;
+        count++;
+    } else {
+        cout << "Vegetable list is full!" << endl;
+    }
+}
+
+void updateVegetablePrice(Vegetable veg[], int count, int id, double newPrice) {
+    for (int i = 0; i < count; i++) {
+        if (veg[i].itemID == id) {
+            veg[i].pricePerKg = newPrice;
+            cout << "Updated Item ID: " << id << ", New Price per Kg: " << newPrice << endl;
+            return;
+        }
+    }
+    cout << "Item ID: " << id << " not found." << endl;
+}
+
+void deleteVegetable(Vegetable veg[], int& count, int id) {
+    for (int i = 0; i < count; i++) {
+        if (veg[i].itemID == id) {
+            for (int j = i; j < count - 1; j++) {
+                veg[j] = veg[j + 1];
+            }
+            count--;
+            cout << "Deleted Item ID: " << id << endl;
+            return;
+        }
+    }
+    cout << "Item ID: " << id << " not found." << endl;
+}
+
+void itemsAbove500(int count, Vegetable veg[]) {
+    int num =0;
+    
+    for(int i=0;i<count;i++){
+        if(veg[i].pricePerKg>500){
+            num++;
+        }
+    }
+
+    cout << "Total items above 500: " << num << endl;
+    cout << "Items are: " << endl;
+    for(int i=0;i<count;i++){
+        if(veg[i].pricePerKg>500){
+            cout << "Item ID: " << veg[i].itemID << ", ";
+        }
+    }
+}
+
 int main() {
 
     Vegetable vegetables[10] = {
@@ -26,6 +79,10 @@ int main() {
     int count = 5;
 
     displayVegetableInfo(count, vegetables);
+    addVegetable(vegetables, count, 1010, 300.00);
+    updateVegetablePrice(vegetables, count, 1007, 800.00);
+    deleteVegetable(vegetables, count, 1004);
+    itemsAbove500(count, vegetables);
 
     return 0;
 }
